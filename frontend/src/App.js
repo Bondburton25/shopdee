48fae10abe268d8046f67a1ecac6d3ed8761deea
Bondburton25 <kraiwit.dev@gmail.com>
2022-11-01 20:22:06 +0700

    listing product from data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,34 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Shopdee</a>
       </header>
-      <main>List product</main>
+      <main>
+        <h1>List product</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div key={product.slug} className="product">
+              <a href={`/products/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/products/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button type="button" className="btn btn-primary">
+                  Add to cart
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,44 @@
+const data = {
+  products: [
+    {
+      name: 'Manchester United Home Shirt 2022-23',
+      slug: 'manchester-united-home-shirt-2022-23',
+      category: 'Football Kits',
+      image: '/images/manchester-united-home-shirt-2022-23.webp',
+      price: 70.0,
+      countInStock: 1000,
+      brand: 'Adidas',
+      ratting: 4.5,
+      numReviews: 120,
+      description:
+        "Turned up or pressed down, the humble polo collar has played a starring role in many of Manchester United's biggest moments. Making a comeback on this adidas football jersey, it joins a shield-style badge and engineered pinstripe graphic to produce an eye-catching look. Moisture-absorbing AEROREADY and mesh panels make it a comfortable choice for passionate supporters. Made with 100% recycled materials, this product represents just one of our solutions to help end plastic waste",
+    },
+    {
+      name: 'Manchester United Away Shirt 2022-23',
+      slug: 'manchester-united-away-shirt-2022-23',
+      category: 'Football Kits',
+      image: '/images/manchester-united-away-shirt-2022-23.jpeg',
+      price: 70,
+      countInStock: 1000,
+      brand: 'Adidas',
+      ratting: 4.5,
+      numReviews: 120,
+      description:
+        "They're famous for wearing red. But Manchester United have had more than a few glory nights in white. With its subtle allover design, shield badge and red-and-black 3-Stripes, this adidas away football jersey adds to that rich legacy. Moisture-wicking AEROREADY and mesh panels make it a comfortable wear for supporters. Made with 100% recycled materials, this product represents just one of our solutions to help end plastic waste.",
+    },
+    {
+      name: 'Manchester United Third Shirt 2022-23',
+      slug: 'manchester-united-third-shirt-2022-23',
+      category: 'Football Kits',
+      image: '/images/manchester-united-third-shirt-2022-23.webp',
+      price: 70.0,
+      countInStock: 1000,
+      brand: 'Adidas',
+      ratting: 3.5,
+      numReviews: 120,
+      description:
+        "Created to catch the eye at rivals' grounds. This adidas Manchester United third jersey highlights electrifying away performances with bold colours and a dynamic, '90s-style graphic. Shared with the home and away kits, an embroidered shield badge takes pride of place on the chest. Behind the brilliance, moisture-absorbing AEROREADY keeps football fans comfortable. Made with 100% recycled materials, this product represents just one of our solutions to help end plastic waste.",
+    },
+  ],
+};
+export default data;
